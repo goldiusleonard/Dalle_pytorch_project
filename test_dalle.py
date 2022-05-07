572bffd2280b90c1cdf94bd253c2cfab8b1211d6
Goldius Leonard <57057496+goldiusleonard@users.noreply.github.com>
2022-05-07 08:48:25 +0000

    Add files via upload

--- test_dalle.py
+++ test_dalle.py
@@ -47,23 +47,22 @@ vae = DiscreteVAE(
     image_size = 256,
     num_layers = 3,           # number of downsamples - ex. 256 / (2 ** 3) = (32 x 32 feature map)
     num_tokens = 8192,        # number of visual tokens. in the paper, they used 8192, but could be smaller for downsized projects
-    codebook_dim = 512,       # codebook dimension
+    codebook_dim = 1024,      # codebook dimension
     hidden_dim = 64,          # hidden dimension
     num_resnet_blocks = 1,    # number of resnet blocks
-    temperature = 0.9,        # gumbel softmax temperature, the lower this is, the harder the discretization
-    straight_through = False, # straight-through for gumbel softmax. unclear if it is better one way or the other
+    temperature = 0.9         # gumbel softmax temperature, the lower this is, the harder the discretization
 ).to(device)
 
 dalle = DALLE(
     dim = 1024,
-    vae = vae,                                  # automatically infer (1) image sequence length and (2) number of image tokens
-    num_text_tokens = 10000,                    # vocab size for text
-    text_seq_len = 256,                         # text sequence length
-    depth = 1,                                  # should aim to be 64
-    heads = 16,                                 # attention heads
-    dim_head = 64,                              # attention head dimension
-    attn_dropout = 0.1,                         # attention dropout
-    ff_dropout = 0.1                            # feedforward dropout
+    vae = vae,                  # automatically infer (1) image sequence length and (2) number of image tokens
+    num_text_tokens = 49408,    # vocab size for text
+    text_seq_len = 256,         # text sequence length
+    depth = 1,                  # should aim to be 64
+    heads = 16,                 # attention heads
+    dim_head = 64,              # attention head dimension
+    attn_dropout = 0.1,         # attention dropout
+    ff_dropout = 0.1            # feedforward dropout
 ).to(device)
 
 dalle.load_state_dict(torch.load(dalle_load_path))
--- train_dalle.py
+++ train_dalle.py
@@ -2,7 +2,6 @@ import torch
 from torchvision import transforms as T
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from pathlib import Path
 import os
 from tqdm import tqdm
 from dalle_pytorch import DiscreteVAE, DALLE
@@ -13,7 +12,7 @@ from torchvision.datasets.coco import CocoCaptions
 input_image_size = 256
 
 # Change your batch size here
-batch_size = 1
+batch_size = 4
 
 # Change your epoch here
 epoch = 5
@@ -54,12 +53,12 @@ train_data = CocoCaptions(
 
 vae = DiscreteVAE(
     image_size = 256,
-    num_layers = 3,           # number of downsamples - ex. 256 / (2 ** 3) = (32 x 32 feature map)
-    num_tokens = 8192,        # number of visual tokens. in the paper, they used 8192, but could be smaller for downsized projects
-    codebook_dim = 512,       # codebook dimension
-    hidden_dim = 64,          # hidden dimension
-    num_resnet_blocks = 1,    # number of resnet blocks
-    temperature = 0.9         # gumbel softmax temperature, the lower this is, the harder the discretization
+    num_layers = 3,
+    num_tokens = 8192,
+    codebook_dim = 1024,
+    hidden_dim = 64,
+    num_resnet_blocks = 1,
+    temperature = 0.9
 ).to(device)
 
 if os.path.exists(vae_save_path):
@@ -95,27 +94,31 @@ for curr_epoch in range(epoch):
         else:
             iter_idx = range(batch_idx, batch_idx+batch_size, 1)
 
-        image_list = []
-        
+        batch_len = 0
+        total_loss = torch.tensor(0., device=device)
+
         for curr_idx in iter_idx:
-            image, _ = train_data[curr_idx]
-            image = image.unsqueeze(0)
+            image, target = train_data[curr_idx]
+            image = image.unsqueeze(0).type(torch.FloatTensor).to(device)
 
-            image_list.append(image)
-
-        images = torch.cat(image_list, dim=0).type(torch.FloatTensor).to(device)
+            if total_loss == torch.tensor(0., device=device):
+                total_loss = vae(image, return_loss=True)
+            else:
+                total_loss += vae(image, return_loss=True)
+            batch_len += 1
+                
+        avg_loss = total_loss / batch_len
 
         opt.zero_grad()
-        loss = vae(images, return_loss = True)
-        loss.backward()
+        avg_loss.backward()
         opt.step()
 
         if batch_idx != 0 and batch_idx % 100 == 0:
             torch.save(vae.state_dict(), vae_save_path)
-            sched.step(loss)
+            sched.step(avg_loss)
         
-        if batch_idx % 1000 == 0:
-            print(f"loss: {loss.data}")
+        if batch_idx % 100 == 0:
+            print(f"loss: {avg_loss.data}")
 
 torch.save(vae.state_dict(), vae_save_path)
 
@@ -123,14 +126,14 @@ tokenizer = SimpleTokenizer()
 
 dalle = DALLE(
     dim = 1024,
-    vae = vae,                                  # automatically infer (1) image sequence length and (2) number of image tokens
-    num_text_tokens = 10000,                    # vocab size for text
-    text_seq_len = 256,                         # text sequence length
-    depth = 1,                                  # should aim to be 64
-    heads = 16,                                 # attention heads
-    dim_head = 64,                              # attention head dimension
-    attn_dropout = 0.1,                         # attention dropout
-    ff_dropout = 0.1                            # feedforward dropout
+    vae = vae,                  # automatically infer (1) image sequence length and (2) number of image tokens
+    num_text_tokens = 49408,    # vocab size for text
+    text_seq_len = 256,         # text sequence length
+    depth = 1,                  # should aim to be 64
+    heads = 16,                 # attention heads
+    dim_head = 64,              # attention head dimension
+    attn_dropout = 0.1,         # attention dropout
+    ff_dropout = 0.1            # feedforward dropout
 ).to(device)
 
 if os.path.exists(dalle_save_path):
@@ -165,44 +168,32 @@ for curr_epoch in range(epoch):
         else:
             iter_idx = range(batch_idx, batch_idx+batch_size, 1)
 
-        image_list = []
-        text_list = []
-        
+        batch_len = 0
+        total_loss = torch.tensor(0., device=device)
+
         for curr_idx in iter_idx:
             image, target = train_data[curr_idx]
-            image = image.unsqueeze(0)
-            text = tokenizer.tokenize(target)
+            image = image.unsqueeze(0).type(torch.FloatTensor).to(device)
+            texts = tokenizer.tokenize(target).type(torch.LongTensor).to(device)
 
-            text_size = len(text)
-            for i in range(text_size):
-                image_list.append(image)
-            
-            text_list.append(text)
-
-        text = torch.cat(text_list, dim=0).to(device)
-        image = torch.cat(image_list, dim=0).to(device)
+            for text in texts:
+                if total_loss == torch.tensor(0., device=device):
+                    total_loss = dalle(text.unsqueeze(0), image, return_loss=True)
+                else:
+                    total_loss += dalle(text.unsqueeze(0), image, return_loss=True)
+                batch_len += 1
+                
+        avg_loss = total_loss / batch_len
 
         opt.zero_grad()
-        loss = dalle(text, image, return_loss = True)
-        loss.backward()
+        avg_loss.backward()
         opt.step()
 
         if batch_idx != 0 and batch_idx % 100 == 0:
             torch.save(dalle.state_dict(), dalle_save_path)
-            sched.step(loss)
+            sched.step(avg_loss)
         
-        if batch_idx % 1000 == 0:
-            print(f"loss: {loss.data}")
+        if batch_idx % 100 == 0:
+            print(f"average loss: {avg_loss.data}")
 
 torch.save(dalle.state_dict(), dalle_save_path)
-
-test_inputs = ['Closeup of bins of food that include broccoli and bread.'] # text input for the model (can be more than one)
-
-text = tokenizer.tokenize(test_inputs).to(device)
-
-test_img_tensors = dalle.generate_images(text)
-
-for test_idx, test_img_tensor in enumerate(test_img_tensors):
-    test_img = T.ToPILImage()(test_img_tensor)
-    test_save_path = os.path.join(test_img_save_path, f"{test_inputs[test_idx]}.jpg")
-    test_img.save(Path(test_save_path))
